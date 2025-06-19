923e894c52aa5fd1fab67192a0b0ae333205841e
PurviMurkute <purvimurkute51@gmail.com>
2025-06-19 16:28:04 +0530

    financial summary added

---
+++ client/src/components/FinancialSummaryCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const FinancialSummaryCard = ({type, amount}) => {
+    
+    let lable = '';
+    let sign = '';
+    let color = '';
+
+    if(type === "income"){
+        lable = "Total Income"
+        sign = "+"
+        color = "text-green-700"
+    }else if(type === "expense"){
+        lable = "Total Expense"
+        sign = "-"
+        color = "text-red-700"
+    }else{
+        lable = "Total Balance"
+        sign = ""
+        color = "text-blue-700"
+    }
+
+  return (
+    <div className='bg-slate-100 flex flex-col justify-center items-center shadow-md rounded-lg p-3 mx-8 mb-5 hover:shadow-lg transition'>
+          <p className={`text-2xl font-bold ${color}`}>{sign} ₹{amount}</p>
+          <h4 className='text-xl font-bold text-slate-900'>{lable}</h4>
+        </div>
+  )
+}
+
+export default FinancialSummaryCard
--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,86 +1,135 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import toast, { Toaster } from 'react-hot-toast';
-import TransactionCard from '../components/TransactionCard';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import logo from './../assets/logo.png';
+import toast, { Toaster } from "react-hot-toast";
+import TransactionCard from "../components/TransactionCard";
+import FinancialSummaryCard from "../components/FinancialSummaryCard";
 
 const Dashboard = () => {
+  const [user, setUser] = useState("");
+  const [transactions, setTransactions] = useState([]);
+  const [netIncome, setNetIncome] = useState(0);
+  const [netExpense, setNetExpense] = useState(0);
 
-  const [ user, setUser ] = useState('');
-  const [ transactions, setTransactions ] = useState([]);
-
-  useEffect(()=>{
+  useEffect(() => {
     const currentuser = JSON.parse(localStorage.getItem("currentuser"));
 
-    if(currentuser){
+    if (currentuser) {
       setUser(currentuser);
     }
 
-    if(!currentuser){
-      window.location.href = '/signin';
+    if (!currentuser) {
+      window.location.href = "/signin";
     }
-  }, [])
+  }, []);
 
   const loadTransactions = async () => {
-    if(!user._id) return;
+    if (!user._id) return;
 
-    try{
-    const response = await axios.get(`${import.meta.env.VITE_API_KEY}/transactions?userId=${user._id}`)
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_KEY}/transactions?userId=${user._id}`
+      );
 
-    console.log(response.data.data);
+      console.log(response.data.data);
 
-    if(response.data.success){
-      toast.loading("Loading transactions...");
+      if (response.data.success) {
+        toast.loading("Loading transactions...");
 
-      setTimeout(()=>{
-        setTransactions(response.data.data);
-      }, 3000)
-      toast.dismiss();
-    }else(
-      toast.error(response.data.message)
-    )}catch(e){
+        setTimeout(() => {
+          setTransactions(response.data.data);
+        }, 3000);
+        toast.dismiss();
+      } else toast.error(response.data.message);
+    } catch (e) {
       toast.error(e.response.data.message);
     }
-  }
+  };
 
   const handleSignOut = () => {
     localStorage.clear();
-    toast.success("SignOut Successful")
+    toast.success("SignOut Successful");
 
-    setTimeout(()=>{
-      window.location.href = '/signin';
-    }, 2000)
-  }
+    setTimeout(() => {
+      window.location.href = "/signin";
+    }, 2000);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     loadTransactions();
-  }, [user])
-  return (
-    <div className='bg-slate-300 min-h-screen inset-0 fixed'>
-      <div className='h-[50px] bg-slate-900 w-full top-0 fixed'>
-        <button className='fixed top-2 right-2 bg-red-700 px-3 py-1 m-1 text-slate-100 text-md font-medium' onClick={handleSignOut}>SignOut</button>
-      </div>
-      <h1 className='mt-20 text-3xl font-extrabold text-center text-slate-900 pb-2'>Hello {user.name}👋🏻</h1>
-      <div className='w-[500px] h-[500px] my-5 mx-10 p-2 shadow-xl flex flex-col justify-center overflow-y-scroll bg-white'>
-        <h1 className='text-2xl font-bold text-slate-900 p-4'>Recent Transactions</h1>
-      {
-        transactions.map((transaction, i)=>{
-          const {_id, title, amount, type, category, createdAt} = transaction;
-          return(
-            <TransactionCard 
-            _id={_id}
-            title={title}
-            amount={amount}
-            type={type}
-            category={category}
-            createdAt={createdAt}
-            />
-          )
-        })
-      }
-      </div>
-      <Toaster/>
-    </div>
-  )
-}
+  }, [user]);
 
-export default Dashboard
+  useEffect(() => {
+    let income = 0;
+    let expense = 0;
+
+    transactions.forEach((transaction) => {
+      if (transaction.type == "income") {
+        income += transaction.amount;
+      } else {
+        expense += transaction.amount;
+      }
+
+      setNetIncome(income);
+      setNetExpense(expense);
+    });
+  }, [transactions]);
+
+  return (
+    <div className="bg-slate-300 min-h-screen inset-0 fixed">
+      <div className="h-[50px] bg-slate-900 w-full top-0 fixed">
+        <h1 className="font-bold text-2xl font-serif  text-slate-100 p-2">
+          <img src={logo} alt="logo" className="w-[40px] inline" /> ExpenseDiary
+        </h1>
+        <button
+          className="fixed top-2 right-2 bg-red-700 px-3 py-1 m-1 text-slate-100 text-md font-medium"
+          onClick={handleSignOut}
+        >
+          SignOut
+        </button>
+      </div>
+      <h1 className="mt-20 text-3xl font-extrabold text-center text-slate-900 pb-2">
+        Hello {user.name}👋🏻
+      </h1> 
+      <h3 className="text-2xl font-bold text-center text-slate-900 pb-2">
+        Welcome to ExpenseDiary!😊
+      </h3>
+
+      <div className="flex justify-evenly">
+        <div className="w-[550px] h-[500px] my-5 mx-10 p-2 shadow-xl flex flex-col justify-center bg-white rounded-2xl">
+          <h1 className="text-2xl font-bold text-slate-900 p-4 ms-7 mb-2">
+            Your Financial Summary
+          </h1>
+          <FinancialSummaryCard type="income" amount={netIncome} />
+          <FinancialSummaryCard type="expense" amount={netExpense} />
+          <FinancialSummaryCard
+            type="balance"
+            amount={netIncome - netExpense}
+          />
+        </div>
+        <div className="w-[550px] h-[500px] my-5 mx-10 p-2 shadow-xl flex flex-col justify-center overflow-y-scroll bg-white rounded-2xl">
+          <h1 className="text-2xl font-bold text-slate-900 p-4">
+            Recent Transactions
+          </h1>
+          {transactions.map((transaction, i) => {
+            const { _id, title, amount, type, category, createdAt } =
+              transaction;
+            return (
+              <TransactionCard
+                _id={_id}
+                title={title}
+                amount={amount}
+                type={type}
+                category={category}
+                createdAt={createdAt}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default Dashboard;
